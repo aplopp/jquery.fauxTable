e1b857e7fb849e7850500081bdafd60513c123ad
Anthony Plopper <anthony@anthonyplopper.com>
2016-10-21 15:29:10 -0500

    Refactor jquery.fauxTable.js
    Expose an API, work through all of the code.

--- jquery.fauxTable.js
+++ jquery.fauxTable.js
@@ -6,136 +6,189 @@
 	var $window = $(window);
 	$window.resizedEvent();
 
-	$.fn.fauxTable = function( options ){
-		var that = this;
-		var noop = function(){};
-		var defaults = {
+	function FauxTable($el, args){
+		this.$el = $el;
+		this.settings = $.extend( {}, {
 			cellSelector: '.tb-td',
 			cellInnerSelector: false,
 			classesThatCanChange: [ 'active' ],
 			excludeSelector: false,
-			beforeResize: noop,
-			afterResize: noop
-		};
-		var settings = $.extend( defaults, options );
-		if ( typeof settings.classesThatCanChange === 'string' ){
-			settings.classesThatMayChange = [ settings.classesThatMayChange ];
+			beforeResize: function(){},
+			afterResize: function(){}
+		}, args );
+		
+		if ( typeof this.settings.classesThatCanChange === 'string' ){
+			settings.classesThatMayChange = [ settings.classesThatCanChange ];
 		}
-		this.each( function(){
-	 		var $container = $(this);
- 			var $cells = $(this).children( settings.cellClass );
- 			// one or fewer cells, stop dead
- 			if ( $cells.size() <= 1 ) return;
 
- 			var cellElements = [];
- 			// for each of the cell's children, add their classes to the array
- 			$cells.each( function(){
-
- 				var $innerContainer = settings.cellInnerSelector ? $(this).children( settings.cellInnerSelector ) : false;
- 				if( ! $innerContainer || $innerContainer.size() === 0 ){
- 					var $toAlign = $(this).children();
- 				} else {
- 					var $toAlign = $innerContainer.children();
- 				}
- 				$(this).data('toAlign', $toAlign );
- 				if ( settings.excludeSelector ){
- 					$toAlign = $toAlign.filter( ':not('+settings.excludeSelector+')');
- 				}
- 				$toAlign.each( function(){
- 					var classes = $(this).attr('class' );
- 					var allClasses = classes ? $(this).attr('class').split( /\s+/) : [];
- 					var selector = [ $(this).prop('tagName') ];
- 					for( var i in allClasses ){
- 						if ( allClasses[i] && settings.classesThatCanChange.indexOf( allClasses[i] ) === -1 ){
- 							selector.push( allClasses[i]);
- 						}
- 					}
- 					var selector =  selector.join( '.' );
- 					if ( cellElements.indexOf( selector ) === -1 ){
- 						cellElements.push( selector )
- 					}
- 				});
- 			});
- 			function adjustHeights( $toAdjust, height ){
- 				if ( $toAdjust.size() > 1 ){
- 					$toAdjust.height( height );
- 				}
- 			}
- 			function lineUpCells( e, resize ){
- 				settings.beforeResize( $cells, resize );
- 				var elClass, selector, maxHeight;
-				// record the indexes of each linebreak
-				var lineBreaks = [];
-				var index = 0;
-				var cellHeight = 0;
-				var lastTop = $cells.first().offset().top;
-				$cells.each( function(){
-					if ( $(this).height() > cellHeight ){
-						cellHeight = $(this).height();
+		this.init();
+		this.apiMethods = {
+			destroy: this.destroy,
+			reinit: this.reinit,
+			resize: this.handleResize
+		};
+		$window.bind('resized-w.fauxtable', this, this.handleResize );
+		this.handleResize();
+	}
+	FauxTable.prototype.init = function(){
+		this.cellData = [];
+		this.cellElements = [];
+		this.$cells = this.$el.children( this.settings.cellSelector );
+		this.$cells.each( function(e, cell){
+			var $cell = $(cell);
+			var $innerContainer = this.settings.cellInnerSelector ? $cell.children( this.settings.cellInnerSelector ) : false;
+			if( ! $innerContainer || $innerContainer.length === 0 ){
+				var $toAlign = $cell.children();
+			} else {
+				var $toAlign = $innerContainer.children();
+			}
+			if ( this.settings.excludeSelector ){
+				$toAlign = $toAlign.filter( ':not('+this.settings.excludeSelector+')');
+			}
+			this.cellData.push({
+				$cell: $cell,
+				$toAlign: $toAlign,
+				lineBreak: null
+			});
+			$toAlign.each( function(e, el){
+				var classes = $(el).attr('class' );
+				var allClasses = classes ? $(el).attr('class').split( /\s+/) : [];
+				var selector = [ $(el).prop('tagName') ];
+				for( var i in allClasses ){
+					if ( allClasses[i] && this.settings.classesThatCanChange.indexOf( allClasses[i] ) === -1 ){
+						selector.push( allClasses[i]);
 					}
-				});
-				$cells.css({ height: cellHeight });
-				$cells.each( function(){
-					var topPos = $(this).offset().top;
-					if ( topPos > lastTop ){
-						if ( index !== 0 ){
-							lineBreaks.push( true );
-						} else {
-							lineBreaks.push( false )
-						}
-						lastTop = topPos;
-					} else {
-						lineBreaks.push( false );
-					}
-					index++;
-				});
-				$cells.css({ height: '' });
- 				for( i in cellElements ){
- 					maxHeight = 0;
- 					selector = cellElements[i];
- 					var $toAdjust = $();
- 					var lastTop = 0;
- 					var index = -1;
- 					$cells.each( function(){
- 						index++;
-		 				var $toAlign = $(this).data('toAlign').filter( selector );
-		 				// if just a tag name
-		 				if ( selector.indexOf( '.' ) === -1 && selector.indexOf( '#') === -1 ){
-			 				for ( j in cellElements ){
-			 					if ( cellElements[j] !== selector ){
-			 						var thisSelector = cellElements[j];
-			 						if ( thisSelector.substr( 0, thisSelector.regexIndexOf(/[#\.]/) ) === selector ){
-			 							$toAlign = $toAlign.not( thisSelector );
-			 						}
-			 					}
-			 				}
-		 				}
-		 				// if no elements to align, return
-		 				if ( $toAlign.size() === 0 ) return;
- 						// return it to natural height
- 						$toAlign.height( '' );
- 						// otherwise, raise maxHeight if necessary
- 						var height = $toAlign.height();
- 						if ( height > maxHeight ){
- 							maxHeight = height;
- 						}
- 						$toAdjust = $toAdjust.add( $toAlign );
+				}
+				selector = selector.join( '.' );
+				if ( this.cellElements.indexOf( selector ) === -1 ){
+					this.cellElements.push( selector )
+				}
+			}.bind(this));
+		}.bind(this) );
+	}
+	FauxTable.prototype.handleResize = function(e){
+		if ( !this.$el && e.data.$el ){
+			// this object is passed to the event
+			// so the function can be unbound from the window properly when destroyed
+			arguments.callee.apply( e.data );
+			return;
+		}
+		// one or fewer cells, stop dead
+ 		if ( this.$cells.size() <= 1 ) return;
+		
+		this.recordLineBreaks();
+		
+		this.settings.beforeResize( this.cellData, $(window).width() );
 
- 						// if there is a line break
- 						// adjust cells, clear the maxHeight, clear the list of cells to adjust
- 						if ( lineBreaks[ index + 1 ]){
-  							adjustHeights( $toAdjust, maxHeight ); // adjust heights of previous cells
- 							maxHeight = 0; // clear maxHeight
- 							$toAdjust = $(); // and clear
- 							return;
- 						}
- 					});
- 					adjustHeights( $toAdjust, maxHeight ); // adjust heights of any inline cellElements 
- 				}
- 				settings.afterResize( $cells, resize );
- 			}
- 			$container.resizedEvent().on( 'resized-w resized-init', lineUpCells );
- 			lineUpCells();
- 		});
+
+		var selector;
+ 		$.each( this.cellElements, function( i, selector ){
+			var $elementsToAdjust = $();
+			var lastTop = 0;
+			var maxHeightOfElement = 0;
+			$.each( this.cellData, function(i, data){
+	 			var $el = data.$toAlign.filter( selector );
+
+				// if no elements to align, return
+				if ( $el.length === 0 ) return;
+				
+				// return it to natural height
+				$el.height( '' );
+				
+				// raise maxHeight if necessary
+				var h = $el.height();
+				if ( h > maxHeightOfElement ){
+					maxHeightOfElement = h;
+				}
+				$elementsToAdjust = $elementsToAdjust.add( $el );
+				// if there is a line break on the very next cell
+				if ( this.cellData.length >= (i + 1) || this.cellData[ i + 1 ].lineBreak ){
+					// adjust heights of this element in previous cells
+					$elementsToAdjust.height( maxHeightOfElement );
+					// clear the maxHeightOfElement
+					maxHeightOfElement = 0;
+					// clear the list of cells to adjust
+					$toAdjust = $(); // and clear
+				}
+			}.bind(this));
+ 		}.bind(this));
+ 		return this;
+	}
+	FauxTable.prototype.recordLineBreaks = function(){
+		// set every cell to the tallest height there is to
+		// ensure that they all clear each other.
+		var maxNaturalCellHeight = 0;
+		this.$cells.each( function(){
+			if ( $(this).height() > maxNaturalCellHeight ){
+				maxNaturalCellHeight = $(this).height();
+			}
+		});
+		this.$cells.css({ height: maxNaturalCellHeight });
+
+		// record location of all line breaks
+		var previousTopPos = this.$cells.first().offset().top;
+		$.each( this.cellData, function(i, data){
+			var topPos = data.$cell.offset().top;
+			if ( topPos > previousTopPos ){
+				if ( i !== 0 ){
+					data.lineBreak = true;
+				} else {
+					data.lineBreak = false;
+				}
+				previousTopPos = topPos;
+			} else {
+				data.lineBreak = false;
+			}
+		});
+
+		// reset cells to natural height
+		this.$cells.css({
+			height: ''
+		});
+		return this;
+	}
+	FauxTable.prototype.api = function( methodName, arguments ){
+		if ( this.apiMethods.hasOwnProperty( methodName ) ){
+			return this.apiMethods[ methodName ].apply(this, [ arguments ]);
+		}
+		console.warn( 'fauxTable() has no method "'+methodName+'"');
+		return;
+	}
+	FauxTable.prototype.reinit = function(){
+		$(window).unbind( 'resized-w', this.handleResize );
+
+		//reset heights
+		$.each( this.cellData, function(i,data){
+			data.$cell.height( '' );
+			data.$toAlign.height( '' );
+		});
+
+		this.init();
+		this.handleResize();
+		return this;
+	}
+	FauxTable.prototype.destroy = function(){
+		$(window).unbind( 'resized-w.fauxtable', this.handleResize );
+		this.$el.data('fauxtable', null);
+		//reset heights
+		$.each( this.cellData, function(i,data){
+			data.$cell.height( '' );
+			data.$toAlign.height( '' );
+		});
+		return true;
+	}
+	$.fn.fauxTable = function( options ){
+		return this.each( function(){
+			if ( typeof( options ) === 'string' ){
+				var methodName = options;
+				if ( $(this).data('fauxtable') ){
+					return $(this).data('fauxtable').api(methodName, arguments[1] );
+				} else {
+					console.warn( 'Attempted to run .fauxTable("'+options+'") but FauxTable is not initialized on this element' );
+					return;
+				}
+			}
+			$(this).data('fauxtable', new FauxTable($(this), options) );
+		});
 	}
 }( jQuery, window ) );
